7364535aae4ecaec2a8cc8d96b8654d72b9f55ab
APKAVIFL7EWRG4EKB4U4 <ito.shun@pi-pe.co.jp>
2024-08-07 11:36:55 +0900

    ディレクトリの構成を変更

---
+++ src/FormGenerate/require.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Collection/require.php';
+require_once 'SpiralConnecter/require.php';
+require_once 'SiValidator2/require.php';
+require_once 'FormGenerate/Renderer/Bootstrap5Renderer.php';
+require_once 'FormGenerate/Renderer/DefaultRenderer.php';
+require_once 'FormGenerate/Helper/SanitizeHelper.php';
+require_once 'FormGenerate/Helper/FieldRenderer.php';
+require_once 'FormGenerate/FormBuilder.php';
